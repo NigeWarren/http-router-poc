87d680f8811505c4711570296769ec4586a1532e
NigeWarren <Nige@zink-digital.com>
2018-03-20 17:56:55 +0000

    Killswitch on Source stream with resource clean up

--- src/main/scala/io/mewbase/Client.scala
+++ src/main/scala/io/mewbase/Client.scala
@@ -21,16 +21,16 @@ object Client extends App with Port {
   val subs1 = s"http://$getHostName:$getPortNumber/subscribe/bbc1"
   val subs2 = s"http://$getHostName:$getPortNumber/subscribe/bbc2"
 
-
   val entity  = """{ "name" : "Fred" }"""
 
-
+  // Tests Simple ping
   val publishRequest = Http().singleRequest(HttpRequest(POST, ping, entity = entity)).map { response =>
     println(response.entity.toString)
   }
 
-  var chunkOne : CompletableFuture[String] = new CompletableFuture()
 
+  // Test subscribe to channel capturing Subs ID in future.
+  var chunkOne : CompletableFuture[String] = new CompletableFuture()
   val sub1F = Http().singleRequest(HttpRequest(POST,  subs1)).flatMap { response =>
     response.entity.dataBytes.runForeach { chunk =>
       if (!chunkOne.isDone()) chunkOne.complete(chunk.utf8String)
@@ -38,22 +38,24 @@ object Client extends App with Port {
     }
   }
 
-  Thread.sleep(1000)
-    val another = Http().singleRequest(HttpRequest(POST, ping, entity = entity)).map { response =>
+  // Test interleave calls to the server
+  Thread.sleep(3000)
+    val anotherPing  = Http().singleRequest(HttpRequest(POST, ping, entity = entity)).map { response =>
       println(response.entity.toString)
     }
 
   Thread.sleep(2500)
 
-
+  // Test more than one subscription concurrently
   val sub2F = Http().singleRequest(HttpRequest(POST,  subs2)).flatMap { response =>
     response.entity.dataBytes.runForeach { chunk =>
       println(chunk.utf8String)
     }
   }
 
-  Thread.sleep(2500)
+  Thread.sleep(6500)
 
+  // Test unsubscribe from one of the concurrent streams
   val subsId = chunkOne.get().split(":")(2)
   val unsubs1 = s"http://$getHostName:$getPortNumber/unsubscribe/"+ subsId
   val unsub = Http().singleRequest(HttpRequest(POST,  unsubs1)).flatMap { response =>
--- src/main/scala/io/mewbase/Server.scala
+++ src/main/scala/io/mewbase/Server.scala
@@ -15,8 +15,12 @@ import akka.http.scaladsl.server.Directives.{entity, _}
 import akka.stream.actor.{ActorPublisher, ActorPublisherMessage}
 import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
 
-import scala.concurrent.Future
+import scala.collection.mutable
+import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success, Try}
+
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 
 object Server extends App with Port
@@ -25,79 +29,39 @@ object Server extends App with Port
   implicit val system = ActorSystem("server")
   implicit val materializer = ActorMaterializer()
 
-
-  //*** Actor Publisher has been deprecated in favour of GraphStage
-  // see
-  case class StringActorPublisher() extends ActorPublisher[String] {
-
-    val counter = new AtomicLong(0)
-
-    val myActorName = self.path.name
-
-
-    def pullElement() : Try[String] = {
-      if (counter.get == 0L) {
-        counter.getAndIncrement
-        Try(myActorName)
-      } else {
-        Thread.sleep(1000)
-        Try(counter.getAndIncrement.toString)
-      }
-    }
-
-    def cleanupResources() = {
-      println("Cleaning up resources");
-    }
-
-
-    import scala.concurrent.ExecutionContext.Implicits.global
-
-
-    def receive = {
-      case ActorPublisherMessage.Request(n) => {
-        println(s"request for $n elements")
-          while (isActive && totalDemand > 0) {
-            pullElement() match {
-              case Success(event) =>
-                onNext(event)
-              case Failure(ex) =>
-                onError(ex)
-            }
-          }
-        }
-
-
-      case ActorPublisherMessage.Cancel => {
-        println("Received cancel message")
-        cleanupResources()
-        onCompleteThenStop()
-      }
-
-      case ActorPublisherMessage.SubscriptionTimeoutExceeded =>
-        cleanupResources()
-    }
-
-  } /* end of deprecated ActorPublisher */
+  // Store a killSwitch associated with each
+  val killSwitches = mutable.Map[String, Promise[Unit]]()
 
 
 
-  class StringSource(id : UUID) extends GraphStage[SourceShape[String]] {
+  class StringSource(id : UUID, killSwitch : Future[Unit]) extends GraphStage[SourceShape[String]] {
     // Define the (sole) output port of this stage
     val out: Outlet[String] = Outlet("StringSource")
+
     // Define the shape of this stage, which is SourceShape with the port we defined above
     override val shape: SourceShape[String] = SourceShape(out)
 
-    var counter = 0
 
     // This is where the actual (possibly stateful) logic will live
     override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
       new GraphStageLogic(shape) {
+
+        var counter = 0
+
+        override def preStart(): Unit = {
+          val callback = getAsyncCallback[Unit] { (_) =>
+            completeStage()
+            println("Cleaning up resources")
+          }
+          killSwitch.foreach(callback.invoke)
+        }
+
         setHandler(out, new OutHandler {
           override def onPull(): Unit = {
             if (counter == 0) push(out, id.toString())
             else push(out, counter.toString())
             counter += 1
-            Thread.sleep(1000);
+            Thread.sleep(1000)
           }
         })
       }
@@ -106,11 +70,9 @@ object Server extends App with Port
 
   def createStringSource(channelName : String) : Source[String,NotUsed]  = {
     val id = UUID.randomUUID
-    //  val publisher  = ActorPublisher[String](system.actorOf(Props(classOf[StringActorPublisher]), actorUUID.toString))
-    //    Source.fromPublisher(publisher).map {
-    //    str => ChunkStreamPart.apply(s"Channel : $channelName EventNumber :$str") }
-
-   Source.fromGraph( new StringSource(id) )
+    val killSwitch = Promise[Unit]()
+    killSwitches put(id.toString, killSwitch)
+    Source.fromGraph( new StringSource(id, killSwitch.future ))
   }
 
 
@@ -147,7 +109,10 @@ object Server extends App with Port
     post {
       path ("unsubscribe" / Segment ) {  subscriptionID =>
         println("unsubscribing " + subscriptionID)
-        system.actorSelection("akka://server/user/"+subscriptionID) ! ActorPublisherMessage.Cancel
+        killSwitches.get(subscriptionID).foreach {  kSwitch =>
+          kSwitch.complete( Try { () => () }  )
+          killSwitches.remove(subscriptionID)
+        }
         complete( "Unsubscribe:"+subscriptionID )
       }
     }
